c8b482d059f6851c1552badd0586a7fe52ed4ce5
vsdavis <vsdavid2005@gmail.com>
2024-07-03 08:48:18 -0300

    Teste RRSoft

--- ApiMercado/Controllers/ProdutosController.cs
+++ ApiMercado/Controllers/ProdutosController.cs
@@ -1,126 +1,130 @@
-﻿using System.Text.Json;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using ApiMercado.Bd;
-using ApiMercado.Models;
+﻿using System.Net;
+using System.Net.Http.Headers; // Adicione esta diretiva para AuthenticationHeaderValue
+using System.Text.Json;
 
-namespace ApiMercado.Controllers
+namespace ApiMercado.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProdutosController : ControllerBase
+    private readonly AppDbContext _context;
+    private readonly HttpClient _httpClient;
+
+    public ProdutosController(AppDbContext context, IHttpClientFactory httpClientFactory)
     {
-        private readonly AppDbContext _context;
-        private readonly HttpClient _httpClient;
+        _context = context;
+        _httpClient = httpClientFactory.CreateClient("MLB");
+    }
 
-        public ProdutosController(AppDbContext context, IHttpClientFactory httpClientFactory)
-        {
-            _context = context;
-            _httpClient = httpClientFactory.CreateClient("MLB");
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Produto>> GetProduto(string id)
-        {
-            try
-            {
-                var produto = await _context.Produto.FindAsync(id);
-
-                if (produto == null)
-                {
-                    return NotFound();
-                }
-
-                return produto;
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Erro ao buscar o produto: {ex.Message}");
-            }
-        }
-
-
-        [HttpPost]
-        public async Task<ActionResult<Produto>> PostProduto(Produto produto)
-        {
-            _context.Produto.Add(produto);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetProduto), new { id = produto.Id }, produto);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutProduto(string id, Produto produto)
-        {
-            if (id != produto.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(produto).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProdutoExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        private bool ProdutoExists(string id)
-        {
-            return _context.Produto.Any(e => e.Id == id);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteProduto(string id)
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Produto>> GetProduto(string id)
+    {
+        try
         {
             var produto = await _context.Produto.FindAsync(id);
+
             if (produto == null)
             {
                 return NotFound();
             }
 
-            _context.Produto.Remove(produto);
-            await _context.SaveChangesAsync();
+            return produto;
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Erro ao buscar o produto: {ex.Message}");
+        }
+    }
 
-            return NoContent();
+
+    [HttpPost]
+    public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+    {
+        _context.Produto.Add(produto);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetProduto), new { id = produto.Id }, produto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutProduto(string id, Produto produto)
+    {
+        if (id != produto.Id)
+        {
+            return BadRequest();
         }
 
-        [HttpGet("MercadoLivre/{id}")]
-        public async Task<IActionResult> GetProdutoDoMercadoLivre(string id)
+        _context.Entry(produto).State = EntityState.Modified;
+
+        try
         {
-            try
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ProdutoExists(id))
             {
-                var url = $"{id}"; // append the id to the base address set in the HttpClient
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
 
-                var response = await _httpClient.GetAsync(url);
+        return NoContent();
+    }
 
-                if (!response.IsSuccessStatusCode)
+    private bool ProdutoExists(string id)
+    {
+        return _context.Produto.Any(e => e.Id == id);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduto(string id)
+    {
+        var produto = await _context.Produto.FindAsync(id);
+        if (produto == null)
+        {
+            return NotFound();
+        }
+
+        _context.Produto.Remove(produto);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpGet("MercadoLivre/{id}")]
+    public async Task<IActionResult> GetProdutoDoMercadoLivre(string id)
+    {
+        try
+        {
+            var url = $"{id}"; // append the id to the base address set in the HttpClient
+
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "APP_USR-6500094978999069-070307-c0edc088225a328a3aae3522dd720a9d-1855456057");
+
+            var response = await _httpClient.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == HttpStatusCode.Unauthorized)
                 {
-                    return StatusCode((int)response.StatusCode, "Erro ao acessar o produto do Mercado Livre");
+                    return StatusCode(401, "Não autorizado para acessar o produto do Mercado Livre");
                 }
-
-                var jsonString = await response.Content.ReadAsStringAsync();
-                var produto = JsonSerializer.Deserialize<ProdutoMercadoLivre>(jsonString);
-
-                return Ok(produto);
+                return StatusCode((int)response.StatusCode, "Erro ao acessar o produto do Mercado Livre");
             }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Erro ao processar a solicitação: {ex.Message}");
-            }
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var produto = JsonSerializer.Deserialize<ProdutoMercadoLivre>(jsonString);
+
+            return Ok(produto);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Erro ao processar a solicitação: {ex.Message}");
         }
     }
 }
